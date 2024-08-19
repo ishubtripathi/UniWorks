6c44c3c66872bfdd65f20e4b59760be2bde6f693
ishubtripathi <ishubtripathi@gmail.com>
2024-08-19 13:20:01 +0530

    some validation

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -4,19 +4,23 @@ const AuthLayout = () => {
   const isAuthenticated = false;
   return (
     <>
-      {
-        isAuthenticated ? (
-          <Navigate to='/' />
-        ) : (
-          <>
-            <section>
-              <Outlet/>
-            </section>
-          </>
-        )
-      }
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          {/* image for login/signup page */}
+          <img 
+          src="/assets/images/side-img.svg" 
+          alt="logo"
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
