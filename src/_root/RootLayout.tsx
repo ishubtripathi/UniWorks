f835b42ad874e2ddf857058def62bda161720b76
ishubtripathi <ishubtripathi@gmail.com>
2024-08-28 01:28:07 +0530

    some more files with some modifications

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import Topbar from '../components/shared/Topbar'
 import LeftSidebar from '../components/shared/LeftSidebar'
-import { Outlet } from 'react-router-dom'
 import Bottombar from '../components/shared/Bottombar'
+
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
